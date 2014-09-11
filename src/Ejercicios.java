f395a0cb10487a9831d62ab0d434deb770366162
Alumno <alumno@localhost.localdomain>
2014-09-11 08:30:00 -0600

    tarea 5

---
+++ src/Ejercicios.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class Ejercicios {
+
+	//Escribe la variable contenido al inicio de un archivo que tiene como nombre nombre_archivo
+	static void escribirString(String nombre_archivo, String contenido)
+	{
+		return;
+	}
+	
+	//Escribe la variable contenido al inicio de un archivo que tiene como nombre nombre_archivo
+	static void escribirInt(String nombre_archivo, int contenido)
+	{
+		return;
+	}
+	
+	//Devuelve el primer entero contenido en el archivo con nombre nombre_archivo
+	static int leerEntero(String nombre_archivo)
+	{
+		return -1;
+	}
+	
+	//Devuelve la primera string contenida en el archivo con nombre nombre_archivo
+	static String leerString(String nombre_archivo)
+	{
+		return "";
+	}
+	
+	//Devuelve el primer double contenido en el archivo con nombre nombre_archivo
+	static double leerDouble(String nombre_archivo)
+	{
+		return -1;
+	}
+	
+	//Devuelve si el archivo con nombre nombre_archivo contiene la string buscada
+	static boolean existe(String nombre_archivo, String buscada)
+	{
+		return false;
+	}
+	
+	//Devuelve el numero menor de un archivo
+	//Dado, el archivo unicamente contiene enteros
+	static int getNumeroMenor(String nombre_archivo)
+	{
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
